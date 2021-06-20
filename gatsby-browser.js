32cc3e99527e71f5baf15185d2e8e998652cfc0e
Michael <animation.weekly.yt@gmail.com>
2021-06-20 16:08:46 +0300

    changed gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,6 @@ import Cursor from "./src/components/Cursor";
 export const wrapPageElement = ({ element }) => {
   return (
     <>
-      <div className="test"></div>
       <Cursor />
       {element}
     </>
